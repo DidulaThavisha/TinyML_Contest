60615eeb5efbda51555511495f39d0ee052288dc
Didula Samaraweera <samaraweeradt.20@uom.lk>
2023-09-15 14:33:37 +0530

    Update pkl2onnx.py

--- pkl2onnx.py
+++ pkl2onnx.py
@@ -1,3 +1,3 @@
 from help_code_demo import pytorch2onnx
 
-pytorch2onnx('./saved_models/IEGM_net.pkl', './saved_models/model_1', 1250)
+pytorch2onnx('/kaggle/working/TinyML_Contest/IEGM_net.pkl', '/kaggle/working/TinyML_Contest/model_1', 1250)
